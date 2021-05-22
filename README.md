1e650e99123c3007927680624b483c61f115afff
Asuki Kono <asukiaaa@gmail.com>
2021-05-22 14:49:43 +0900

    Add initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+# CanBusMCP-arduino (CanBusMCP_asukiaaa)
+
+A library for arduino to communicate with MCP2515 or MCP25625 on SPI to use CAN Bus.
+
+# License
+
+MIT
---
+++ src/CanBusMCP_asukiaaa.hpp
@@ -0,0 +1,9 @@
+#ifndef _CAN_BUS_MCP_ASUKIAAA_HPP_
+#define _CAN_BUS_MCP_ASUKIAAA_HPP_
+
+#include <CanBusData_asukiaaa.h>
+
+namespace CanBusMCP_asukiaaa {
+};
+
+#endif
